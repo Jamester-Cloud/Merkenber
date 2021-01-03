a116a26273d3eaad3f90ffd0568835acf2e2ad27
Jamester <rodriang62@gmail.com>
2021-01-02 19:47:50 -0430

    Agregando vistas. Desarrollando front-end

---
+++ src/components/Inicio.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Inicio extends Component {
+    render() {
+      // Rendering the index page
+        return (
+            <header className="bg-primary text-white">
+              <div className="container text-center">
+                <h1>Bienvenido a merkenber</h1>
+                <p className="lead">Una app pensada para el usuario en su dia a dia</p>
+              </div>
+            </header>
+        )
+    }
+}
+  
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+//Menus
+import NavigationAdmin from './menus/NavigationAdmin';
+import Footer from './Footer';
+import Sidebar from './menus/Sidebar';
+import SidebarClient from './menus/SidebarClient';
+
+export default function PrivateRoute ({component: Component, ...rest}){
+          
+    return (
+        // Show the component only when the user is logged in and is an Administrator
+        // OR Show the component only when the user is logged in and is Client
+        // Otherwise, redirect the user to /signin page
+        <Route {...rest} render={props => (
+                sessionStorage.getItem("isLoggedIn") && sessionStorage.getItem('isLoggedInAdmin')
+                ?   <> {/* React fragment */}
+                        <Sidebar/>
+                        <div className="main-panel" id="main-panel">
+                            <NavigationAdmin/>
+                            <Component {...props}/>
+                            <Footer/>
+                        </div>
+                    </>
+                :sessionStorage.getItem("isLoggedIn") && sessionStorage.getItem('isLoggedInClient')
+                ?
+                    <> {/* React fragment */}
+                        <SidebarClient/>
+                        <div className="main-panel" id="main-panel">
+                            <NavigationAdmin/>
+                            <Component {...props}/>
+                            <Footer/>
+                        </div>
+                    </>
+                :( 
+                    <Redirect to={{
+                        pathname:"/login",
+                        state:{from:props.location}
+                    }} />
+                )
+            )
+        } />
+    );
+}
